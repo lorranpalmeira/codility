39ced0d0b5175d356a4c63f980c164fff95c7eac
loanpal <lorranalvespalmeira@correio.itau.com.br>
2019-05-28 07:31:59 -0300

    Perm Missing Element

---
+++ PermMissingElem/PermMissingElem/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace PermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] A = { 1};
+            //int[] A = {1, 3, 4, 5,9,7,8,6};
+            int[] A = {1, 2,3, 4, 5};
+
+            Console.WriteLine("Result: " + Solution(A));
+        }
+
+        static int Solution(int[] A)
+        {
+            if (A.Length == 0)
+                return 1;
+
+            var ordered = new SortedSet<int>();
+
+            for (int i = 0; i < A.Length; i++)
+                ordered.Add(A[i]);
+
+
+            var last = 0;
+            var finalGap = false;
+
+            foreach (var item in ordered)
+            {
+                if (last + 1 == item)
+                {
+                    finalGap = true;
+                    last = item;
+                    continue;
+                }
+
+                return last + 1;
+            }
+
+            if (finalGap)
+                return last + 1;
+
+
+            return 2;
+        }
+    }
+}
